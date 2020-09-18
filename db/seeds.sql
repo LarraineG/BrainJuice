166181101fba6f0b6a98ca910d29801065e44baf
Calum Held <calumheld@gmail.com>
2020-09-18 13:19:14 -0500

    added humor Listen quotes to seed.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,4 +13,16 @@ VALUES ("Without requirements or design, programming is the art of adding bugs t
 ("Before software can be reusable it first has to be usable.", "Gordon Bell", "Inspiration"),
 ("The best performance improvement is the transition from the nonworking state to the working state.", "J. Osterhout", "Inspiration"),
 ("The trouble with programmers is that you can never tell what a programmer is doing until it’s too late.", "Seymour Cray", "Inspiration"),
-("Don’t worry if it doesn’t work right. If everything did, you’d be out of a job.", "Mosher’s Law of Software Engineering", "Inspiration"),
+("Don’t worry if it doesn’t work right. If everything did, you’d be out of a job.", "Mosher’s Law of Software Engineering", "Inspiration");
+
+INSERT INTO humor(blurb, topic) 
+VALUES("Just open another credit card; it’s free money.","Listen"),
+("If you see a bear and don’t have time to run away, hug it. Bears cannot scratch their stomachs.","Listen"),
+("Trust me. You can dance.","Listen"),
+("You can scoop icecream easier if you microwave the spoon first.","Listen"),
+("Women love when you ask about their weight. They like to know you care about their health","Listen"),
+("Tell her to calm down. She'll usually understand she was overreacting.","Listen"),
+("Imitate a foreign person's accent when you talk to them so they feel more at home.","Listen"),
+("During an interview always ask about their sexual harrassment policy. It lets them know you're socially conscious.","Listen"),
+("If you commit a felony, you'll never get jury duty again.","Listen"),
+("Put oil on your rotors so your breaks dont stick in the winter.","Listen");
